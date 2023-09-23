4789f5e5c25a985a4c24ba5f8dee44db91047f11
Sanketh00 <sankethsan007@gmail.com>
2023-09-24 00:24:57 +0530

    f

--- dcrm/settings.py
+++ dcrm/settings.py
@@ -65,7 +65,7 @@ WSGI_APPLICATION = 'dcrm.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'sanketh',
+        'NAME': 'sankethr$default',
         'USER': 'sankethr',
         'PASSWORD': 'root@123',
         'HOST': 'sankethr.mysql.pythonanywhere-services.com',
